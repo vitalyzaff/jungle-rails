acb4a865dc1f120529affbd37c087d2ac93f6d0a
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-06-23 17:50:39 +0000

    admin dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.all
+    @products_number = Product.count
+    @categories_number = Category.count
+  end
+
+  def index
+    @products = Product.all
   end
 end
