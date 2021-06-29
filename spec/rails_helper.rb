87546dd6cc471cf8433856313a9da4b05e737740
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-06-29 17:32:40 +0000

    Feature Spec: Home Page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist" # Add this line to require poltergeist
+# Add this line to require poltergeist
+require "capybara/poltergeist"
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
@@ -12,6 +11,10 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require "capybara/rails"
+require "capybara/rspec"
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
